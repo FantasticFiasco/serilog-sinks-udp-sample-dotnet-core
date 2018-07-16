371eecbcac431942d2373eaeb1e680d5146ec9f4
FantasticFiasco <mattias.kindborg@gmail.com>
2018-07-16 22:08:11 +0200

    feat: add sample application

---
+++ src/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+using System.Threading;
+using Sample.Generators;
+using Serilog;
+using Serilog.Sinks.Udp.TextFormatters;
+
+namespace Sample
+{
+    class Program
+    {
+        static void Main()
+        {
+            ILogger logger = new LoggerConfiguration()
+                .WriteTo.Udp(IPAddress.Loopback, 7071, new Log4jTextFormatter())
+                .WriteTo.Console()
+                .CreateLogger();
+
+            var customerGenerator = new CustomerGenerator();
+            var orderGenerator = new OrderGenerator();
+
+            while (true)
+            {
+                var customer = customerGenerator.Generate();
+                var order = orderGenerator.Generate();
+
+                logger.Information("{@customer} placed {@order}", customer, order);
+
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
