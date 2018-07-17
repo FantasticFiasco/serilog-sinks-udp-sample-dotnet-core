0c87d65e5b104279468b33a8f3a7677c538f0afa
FantasticFiasco <mattias.kindborg@gmail.com>
2018-07-17 08:56:19 +0200

    wip

--- web-api-sample/Program.cs
+++ web-api-sample/Program.cs
@@ -1,17 +1,46 @@
-﻿using Microsoft.AspNetCore;
+﻿using System;
+using System.Net;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Serilog;
+using Serilog.Sinks.Udp.TextFormatters;
 
 namespace Sample
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.Udp(IPAddress.Loopback, 7071, new Log4jTextFormatter())
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting web host");
+
+                CreateWebHostBuilder(args)
+                    .Build()
+                    .Run();
+
+                return 0;
+            }
+            catch (Exception e)
+            {
+                Log.Fatal(e, "Host terminated unexpectedly");
+
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .UseStartup<Startup>();
     }
 }
