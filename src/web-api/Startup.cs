d559170544076bb0de85b023d614a5bf8762f950
FantasticFiasco <mattias.kindborg@gmail.com>
2021-01-02 18:33:52 +0100

    chore: upgrade .net target framework

--- src/web-api/Startup.cs
+++ src/web-api/Startup.cs
@@ -21,21 +21,14 @@ namespace Sample
         {
             services.AddSingleton<ValuesService>();
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services
+                .AddMvc(options => options.EnableEndpointRouting = false)
+                .SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseHsts();
-            }
-
             app.UseHttpsRedirection();
             app.UseMvc();
         }
